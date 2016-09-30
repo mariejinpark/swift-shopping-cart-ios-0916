25555b9e75a3652a95571c25137169fd2a50bd17
mariejinpark <mariejinpark@gmail.com>
2016-09-29 22:57:30 -0400

    Shopping Cart Lab

--- swift-shopping-cart/Cart.swift
+++ swift-shopping-cart/Cart.swift
@@ -7,6 +7,59 @@
 //
 
 class Cart {
+    var items: [Item] = []
     
+    func totalPriceInCents()->Int {
+       var totalCostForItems = Int()
+        
+        for item in items {
+            totalCostForItems += item.priceInCents
+        }
+        return totalCostForItems
+    }
     
+    func add (item:Item) {
+        items.append(item)
+    }
+    
+    func remove(item:Item){
+        for (index, value) in items.enumerated() {
+            if value == item {
+            items.remove (at: index)
+            }
+        }
+    }
+    
+    func items(withName name:String) -> [Item] {
+        var collectedNames = [Item]()
+        
+        for item in items {
+            if item.name == name {
+                collectedNames.append(item)
+            }
+        }
+        return collectedNames
+    }
+    
+    func items(withMinPrice price:Int) -> [Item] {
+        var collectedItems = [Item]()
+        
+        for item in items {
+            if item.priceInCents >= price {
+                collectedItems.append(item)
+            }
+        }
+        return collectedItems
+    }
+    
+    func items(withMaxPrice price:Int) -> [Item] {
+        var collectedItems = [Item]()
+        
+        for item in items {
+            if item.priceInCents <= price {
+                collectedItems.append(item)
+            }
+        }
+    return collectedItems
+    }
 }
